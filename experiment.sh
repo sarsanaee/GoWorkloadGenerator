e8b63e859177adfcf155cc61f61de5de16157450
Alireza <sarsanaee@gmail.com>
2018-08-09 13:18:03 +0430

    rates

--- experiment.sh
+++ experiment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in `seq 1 10`;
+for i in `seq 1 5 90`;
 do
-	./run.bash $i 10.254.254.1 ~/quic_results/tcp 100 5000
+	./run.bash $i 10.254.254.1 ~/quic_results/tcp 100 10000
 done
