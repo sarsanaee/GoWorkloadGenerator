b0018e4bb468ff353f25d605f1e42b1829a16b7e
alireza <sarsanaee@gmail.com>
2018-08-10 18:52:16 +0430

    tuning parameters

--- experiment.sh
+++ experiment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in `seq 1 5 90`;
+for i in `seq 1 5 63`;
 do
-	./run.bash $i 10.254.254.1 ~/quic_results/tcp 100 10000
+	./run.bash $i 10.254.254.239 ~/quic_results/tcp 30 10000
 done
