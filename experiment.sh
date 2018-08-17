7fe0ede9b79c74dae82be5f04b7c9c970de7a35a
Alireza <sarsanaee@gmail.com>
2018-08-17 14:35:22 +0430

    let experiment with less rate!

--- experiment.sh
+++ experiment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in `seq 30 1 40`;
+for i in `seq 30 1 50`;
 do
-	./run.sh $i 10.254.254.239 ~/quic_results/tcp 30 5000
+	./run.sh $i 10.254.254.239 ~/quic_results/tcp 30 1000
 done
